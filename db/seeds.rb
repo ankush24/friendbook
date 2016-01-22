1d3edb783adc4e052e63241e1f1290c0397395a0
Ankush <ankush24gupta@gmail.com>
2016-01-22 20:33:24 +0530

    Creating Heroku Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
- roles = Role.create([{name: 'Voter'}, {name: 'Commenter'}])
+ # roles = Role.create([{name: 'Voter'}, {name: 'Commenter'}])
 
 # users = User.order(:created_at).take(6)
 # 50.times do
 #   content = Faker::Lorem.sentence(5)
 #   users.each { |user| user.microposts.create!(content: content) }
 # end
-# User.create!(username: "Demo-Commenter",
-# 						 email: "commenter@test.com",
-#              password:              "password",
-#              password_confirmation: "password",
-#              role_id: "Commenter")
+User.create!(username: "Demo-Commenter",
+						 email: "commenter@test.com",
+             password:              "password",
+             password_confirmation: "password",
+             role_id: "Commenter")
 
-# 50.times do |n|
-# 	username = "Demo_Commenter-#{n+1}"
-#   email = "commenter-#{n+1}@railstutorial.org"
-#   password = "password"
-#   role = "Commenter"
-#   User.create!(username: username,
-#   						 email: email,
-#                password:              password,
-#                password_confirmation: password,
-#                role_id: role)
-# end
+50.times do |n|
+	username = "Demo_Commenter-#{n+1}"
+  email = "commenter-#{n+1}@railstutorial.org"
+  password = "password"
+  role = "Commenter"
+  User.create!(username: username,
+  						 email: email,
+               password:              password,
+               password_confirmation: password,
+               role_id: role)
+end
